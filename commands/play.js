d44a16e2a2a8d5db74e782566d78410a49cf309b
Mukund Kumar <banananarick69@gmail.com>
2022-11-17 17:19:05 +0530

    base functionality added

--- commands/play.js
+++ commands/play.js
@@ -2,52 +2,15 @@ const { joinVoiceChannel, createAudioPlayer, NoSubscriberBehavior, createAudioRe
 const { SlashCommandBuilder, ChannelType, EmbedBuilder } = require('discord.js');
 const RadioBrowser = require('radio-browser');
 
-function embed_build(result, n) {
-    const embed = new EmbedBuilder()
-        .setColor(0x0099FF)
-        .setTitle(result[n].name)
-        .setURL(result[n].homepage)
-        .addFields(
-            { name: 'Location', value: result[n].country },
-            { name: 'Language', value: result[n].language },
-            { name: 'Votes', value: result[n].votes },
-        )
-        .setTimestamp();
-
-    return embed;
-}
-
-function playTopFive() {
+function apiCall(stationName) {
     const filter = {
-        by: 'topvote',
-        limit: 5,
+        name: stationName,
     };
 
     const start = async () => {
         try {
-            const data = await RadioBrowser.getStations(filter);
+            const data = await RadioBrowser.searchStations(filter);
             return data;
-        } catch (e) {
-            console.log(e);
-        }
-    };
-
-    return start();
-}
-
-function playByCountry(country) {
-    const filter = {
-        by: 'country',
-        searchterm: country,
-        order: 'name',
-        limit: 5,
-    };
-
-    const start = async () => {
-        try {
-            const data = await RadioBrowser.getStations(filter);
-            const streamUrl = data[0].url_resolved;
-            return streamUrl;
         } catch (e) {
             console.log(e);
         }
@@ -61,28 +24,29 @@ module.exports = {
         .setName('play').setDescription('Play a radio channel.')
         // Sort by Country subcommand
         .addSubcommand(subcommand =>
-            subcommand.setName('country').setDescription('Filters stations by country.')
+            subcommand.setName('search').setDescription('Search a station.')
                 .addStringOption(option =>
-                    option.setName('country').setDescription('Enter country.'))
+                    option.setName('name').setDescription('Enter the stations\'s name.'))
                 .addChannelOption(option =>
-                    option.setName('channel').setDescription('Selects channel to join.')
+                    option.setName('channel').setDescription('Select a channel to join.')
                         .addChannelTypes(ChannelType.GuildVoice)),
         )
         // Play one of top five sub command.
         .addSubcommand(subcommand =>
             subcommand.setName('top_five').setDescription('Filters stations by Top Votes')
                 .addIntegerOption(option =>
-                    option.setName('position').setDescription('specify one of top five voted channels.'))
+                    option.setName('input').setDescription('(Integer) Choose one of the top five stations.').setMinValue(1).setMaxValue(5))
                 .addChannelOption(option =>
-                    option.setName('channel').setDescription('Selects channel to join.')
+                    option.setName('channel').setDescription('Select a channel to join.')
                         .addChannelTypes(ChannelType.GuildVoice)),
         ),
 
     async execute(interaction) {
-        if (interaction.options.getSubcommand() === 'country') {
-            const country = interaction.options.getString('country');
+        // search
+        if (interaction.options.getSubcommand() === 'search') {
+            const searchTerm = interaction.options.getString('name');
             const voiceChannel = interaction.options.getChannel('channel');
-            const print = playByCountry(country);
+            const print = apiCall(searchTerm);
             const voiceConnection = joinVoiceChannel({
                 channelId: voiceChannel.id,
                 guildId: interaction.guildId,
@@ -93,17 +57,48 @@ module.exports = {
                     noSubscriber: NoSubscriberBehavior.Play,
                 },
             });
-            print.then(function (result) {
-                interaction.reply(result);
-                const resource = createAudioResource(result);
-                player.play(resource, { inputtype: StreamType.WebmOpus });
-                voiceConnection.subscribe(player);
+
+            print.then(async function (result) {
+                try {
+                    const Title = result[0].name;
+                    const Author = { name: result[0].name, iconURL: 'https://ibb.co/rdK4ZBf', url: result[0].homepage };
+                    const Location = result[0].country;
+                    const Language = result[0].language;
+                    const Votes = result[0].votes;
+
+                    const embed = new EmbedBuilder()
+                        .setColor(0x0099FF)
+                        .setTitle(`Now Playing; ${Title}`)
+                        .setAuthor(Author)
+                        .addFields(
+                            { name: 'Location', value: `${Location}` },
+                            { name: 'Language', value: `${Language}` },
+                            { name: 'Votes', value: `${Votes}` },
+                        )
+                        .setTimestamp();
+
+                    await interaction.reply({ embeds: [embed] });
+                    const resource = createAudioResource(result[0].url_resolved);
+                    player.play(resource, { inputtype: StreamType.WebmOpus });
+                    voiceConnection.subscribe(player);
+                } catch (e) {
+                    // console.log(e);
+                    await interaction.reply(`${searchTerm} is not a valid radio station. Please input a valid one.`);
+                }
             });
         }
+        // Top Five
         else if (interaction.options.getSubcommand() === 'top_five') {
-            let n = interaction.options.getInteger('positon'); n--;
+            const n = interaction.options.getInteger('input');
+            const radioObj = {
+                1: 'MANGORADIO',
+                2: 'REYFM - #original (64kbps)',
+                3: 'Dance Wave!',
+                4: 'Radio Mirchi',
+                5: 'Radio Paradise (320k)',
+            };
             const voiceChannel = interaction.options.getChannel('channel');
-            const print = playTopFive();
+            const print = apiCall(radioObj[n]);
             const voiceConnection = joinVoiceChannel({
                 channelId: voiceChannel.id,
                 guildId: interaction.guildId,
@@ -114,31 +109,34 @@ module.exports = {
                     noSubscriber: NoSubscriberBehavior.Play,
                 },
             });
+
             print.then(async function (result) {
-                const Title = result[2].name;
-                const Image = result[2].favicon;
-                const URL = result[2].homepage;
-                const Location = result[2].country;
-                const Language = result[2].language;
-                const Votes = result[2].votes;
+                try {
+                    const Title = result[0].name;
+                    const Author = { name: result[0].name, iconURL: 'https://ibb.co/rdK4ZBf', url: result[0].homepage };
+                    const Location = result[0].country;
+                    const Language = result[0].language;
+                    const Votes = result[0].votes;
 
-                const embed = new EmbedBuilder()
-                    .setColor(0x0099FF)
-                    .setImage(`${Image}`)
-                    .setThumbnail(`${Image}`)
-                    .setTitle(`${Title}`)
-                    .setURL(`${URL}`)
-                    .addFields(
-                        { name: 'Location', value: `${Location}` },
-                        { name: 'Language', value: `${Language}` },
-                        { name: 'Votes', value: `${Votes}` },
-                    )
-                    .setTimestamp();
+                    const embed = new EmbedBuilder()
+                        .setColor(0x0099FF)
+                        .setTitle(`Now Playing; ${Title}`)
+                        .setAuthor(Author)
+                        .addFields(
+                            { name: 'Location', value: `${Location}` },
+                            { name: 'Language', value: `${Language}` },
+                            { name: 'Votes', value: `${Votes}` },
+                        )
+                        .setTimestamp();
 
-                await interaction.reply({ embeds: [embed] });
-                const resource = createAudioResource(result[2].url_resolved);
-                player.play(resource, { inputtype: StreamType.WebmOpus });
-                voiceConnection.subscribe(player);
+                    await interaction.reply({ embeds: [embed] });
+                    const resource = createAudioResource(result[0].url_resolved);
+                    player.play(resource, { inputtype: StreamType.WebmOpus });
+                    voiceConnection.subscribe(player);
+                } catch (e) {
+                    console.log(e);
+                    await interaction.reply('The selected station is not available for play. Please try again later.');
+                }
             });
         }
     },
