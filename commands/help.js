0faf342885b2729712dad43e80daedf44c8a1e51
Mukund Kumar <banananarick69@gmail.com>
2022-11-18 21:56:50 +0530

    updated help.js

--- commands/help.js
+++ commands/help.js
@@ -7,15 +7,14 @@ module.exports = {
             .setColor(0x0099FF)
             .setTitle('Help')
             .setDescription('Radiopo is a bot to play your favourite Radio Stations from the comfort of your Discord Server.')
-            .setAuthor({ name: 'Radiopo', iconURL:'https://i.postimg.cc/CxqQBm64/radiopo-logo-2.png', url: 'https://radiopo.xyz' })
+            .setAuthor({ name: 'Radiopo', iconURL: 'https://i.postimg.cc/CxqQBm64/radiopo-logo-2.png', url: 'https://radiopo.xyz' })
             .addFields(
-                { name: '`/play search`', value: 'Search and play a station of your choice.' },
-                { name: 'Directory of Station Names:', value: '[Here](https://www.radio-browser.info/countries)' },
+                { name: '`/play search`', value: 'Search and play a station of your choice. [**Station Names**](https://www.radio-browser.info/countries)' },
                 { name: '`/play top_five`', value: 'Play one of the top voted stations.' },
                 { name: '`/exit`', value: 'Disconnects the bot from active voice channel.' },
                 { name: '`/ping`', value: 'Pings the Radio Browser API for statistics.' },
-                { name: 'Visit the website.', value: 'https://radiopo.xyz', inline: true },
-                { name: 'Report Issues.', value: '[Here](https://github.com/mukund-ks/radiopo/issues)', inline: true },
+                { name: '\u200B', value: '[**Visit the website**](https://radiopo.xyz)', inline: true },
+                { name: '\u200B', value: '[**Report Issues**](https://github.com/mukund-ks/radiopo/issues)', inline: true },
             )
             .setImage('https://i.postimg.cc/pL5Hm2Dh/radiopo-logo.png')
             .setFooter({ text: 'This bot was made by: github.com/mukund-ks', iconURL: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' });
