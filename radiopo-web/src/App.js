09735684d69ae4bfe5c2d668304816779ba296d6
Mukund Kumar <banananarick69@gmail.com>
2022-11-22 23:06:54 +0530

    react base for website

---
+++ radiopo-web/src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://radiopo.xyz"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          This is going to be the radiopo website.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
