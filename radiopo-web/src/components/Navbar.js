373edb5b227117160d8d1e40e8cac823723f0b3a
Mukund Kumar <banananarick69@gmail.com>
2022-11-23 22:22:07 +0530

    updated navbar styling

--- radiopo-web/src/components/Navbar.js
+++ radiopo-web/src/components/Navbar.js
@@ -2,23 +2,27 @@ import './NavbarStyles.css';
 
 function Navbar(){
     return(
-        <nav className='Navbar'>
-            <a href='/' className='siteTitle'>
-                <img  className='radiopoLogo' src='https://i.postimg.cc/vBhkLgP6/radiopo-logo-transparent-white.png' alt='radiopo-logo'></img>
-                Radiopo
-            </a>
-            <ul>
-                <button className='btn'>
-                    <a href='/'>Home</a>
-                </button>
-                <button className='btn'>
-                    <a href='/commands'>Commands</a>
-                </button>
-                <button className='btn'>
-                    <a href='/about'>About</a>
-                </button>
-            </ul>
-        </nav>
+        <div className='Navbar-container'>
+            <div className='logo-container'>
+                <img  className='radiopoLogo' src='https://i.postimg.cc/jdHNBtrZ/radiopo-logo-transparent-white-2.png' alt='radiopo-logo'></img>
+                <a href='/' className='siteTitle'>
+                    Radiopo
+                </a>
+            </div>
+            <nav className='Navbar'>
+                <ul>
+                    <button className='btn'>
+                        <a href='/'>Home</a>
+                    </button>
+                    <button className='btn'>
+                        <a href='/commands'>Commands</a>
+                    </button>
+                    <button className='btn'>
+                        <a href='/about'>About</a>
+                    </button>
+                </ul>
+            </nav>
+        </div>
     );
 }
 
