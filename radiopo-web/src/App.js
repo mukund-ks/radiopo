c7889b1f4eef01e312af0bb486b34fca51270f3b
Mukund Kumar <banananarick69@gmail.com>
2022-11-23 21:42:39 +0530

    added footer; updated styles

--- radiopo-web/src/App.js
+++ radiopo-web/src/App.js
@@ -4,16 +4,19 @@ import Footer  from './components/Footer'
 import Home from './pages/Home'
 import Commands from './pages/Commands'
 import About from './pages/About'
+import './App.css'
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/commands' element={<Commands />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
+    <div className='page-container'>
+      <div className='content-wrap'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/commands' element={<Commands />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </div>
       <Footer />
     </div>
   );
--- radiopo-web/src/components/Footer.js
+++ radiopo-web/src/components/Footer.js
@@ -1,6 +1,23 @@
+import './FooterStyles.css';
+
 function Footer(){
     return(
-        <h3>This is the Footer.</h3>
+        <div className='Footer'>
+            <div className='copyright'>
+                <h3>© 2022 Radiopo</h3>
+            </div>
+            <div className='content'>
+                <h3>
+                    <a href='https://discord.com/oauth2/authorize?client_id=1042031895711592520&permissions=1002912081984&scope=bot'>Invite</a>
+                </h3>
+                <h3>
+                    <a href='https://github.com/mukund-ks/radiopo'>Github</a>
+                </h3>
+                <h3>
+                    <a href='https://github.com/mukund-ks/radiopo'>Support</a>
+                </h3>
+            </div>
+        </div>
     );
 }
 
